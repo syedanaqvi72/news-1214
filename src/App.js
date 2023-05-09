8adfad328f6c8bf0ace9e18a0bbd7f51049de4ed
syedanaqvi72 <mahekzehra5@gmail.com>
2023-05-09 11:07:48 -0700

    working

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        my first base component
+      </div>
+    )
+  }
+}
+
+
+
