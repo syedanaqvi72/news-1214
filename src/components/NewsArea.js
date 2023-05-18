4e93f3bec47b7b032ce0d5aaf62d8369c1ca0222
syedanaqvi72 <mahekzehra5@gmail.com>
2023-05-17 19:30:59 -0700

    again

--- src/components/NewsArea.js
+++ src/components/NewsArea.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
-export class NewsArea extends Component { static proptype={ 
-}
+export class NewsArea extends Component
+{ 
+
+
   render() {
     return (
-      <div className='container my-3'>
+      <div className='container my-4'>
         <h2> News 1214 - Top headlines</h2>
         <div className='row'>
           <div className="col-md-4">
@@ -14,14 +16,15 @@ export class NewsArea extends Component { static proptype={
         <div className="col-md-4">
                 <NewsItem title="my title" description ="my desc"/>
         </div> 
-             <div className="col-md-4">
-                 <NewsItem title="my title" description ="my desc"/>
+             
         </div> 
                <div className="col-md-4">
                  <NewsItem title="my title" description ="my desc"/>
+                 <div className="col-md-4">
+                 <NewsItem title="my title" description ="my desc"/>
         </div>
              </div>
-                  </div>
+                
     )
   }
 }
