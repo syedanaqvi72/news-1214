289b059ae3e3da6f3462132e5dcb40da7638464a
syedanaqvi72 <mahekzehra5@gmail.com>
2023-05-21 08:30:02 -0700

    correction

--- src/components/NewsArea.js
+++ src/components/NewsArea.js
@@ -7,22 +7,25 @@ export class NewsArea extends Component
 
   render() {
     return (
-      <div className='container my-4'>
+      <div className="container my-4">
         <h2> News 1214 - Top headlines</h2>
         <div className='row'>
           <div className="col-md-4">
                <NewsItem title="my title" description ="my desc"/>
         </div>
+
         <div className="col-md-4">
                 <NewsItem title="my title" description ="my desc"/>
         </div> 
              
-        </div> 
                <div className="col-md-4">
                  <NewsItem title="my title" description ="my desc"/>
+                 </div>
+
                  <div className="col-md-4">
                  <NewsItem title="my title" description ="my desc"/>
         </div>
+             </div>
              </div>
                 
     )
