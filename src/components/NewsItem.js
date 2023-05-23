f12e4a07c3116cfa0b9ee6f285ac608dfaba9301
syedanaqvi72 <mahekzehra5@gmail.com>
2023-05-23 09:16:23 -0700

    again changing

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component{
     return (
     <div className='my-3'>
 <div className="card" style={{ width: '18rem' }}>
-  <img src={imageUrl} className="card-img-top" alt="..."/>
+  <img src={!imageUrl:"https://image.cnbcfm.com/api/v1/image/107221976-1680808863244-IMG_8927r.jpg" :imageUrl?} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title"> {title}...</h5>
     <p className="card-text">{description}</p>
