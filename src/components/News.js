ea3dcd2358e1e55ce0fc5f9f9e3f0179a7ef7725
Mehak <mehakzehra5@gmail.com>
2023-06-06 07:18:42 -0700

    again doing

--- src/components/News.js
+++ src/components/News.js
@@ -4,11 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h1>News1214 -- Topheadlines</h1>
+        <div className="row">
+          <div className="col-md-4">
+        <NewsItem title="my title" description="my desc" imageUrl="https://media.cnn.com/api/v1/images/stellar/prod/230603055102-02-india-train-crash-060323.jpg?c=16x9&q=w_800,c_fill"/>
+        </div>
+        <div className="col-md-4">
+        <NewsItem title="my title" description="my desc"/>
+        </div>
+        <div className="col-md-4">
+        <NewsItem title="my title" description="my desc"/>
+        </div>
+      </div>
       </div>
     )
   }
